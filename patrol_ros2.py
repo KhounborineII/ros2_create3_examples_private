b290ae87f2afc3ca026b6d4e1a6080211c4dd30e
KhounborineII <khounborineii@hendrix.edu>
2023-07-20 23:34:24 -0500

    getting out of limbo with this one

--- patrol_ros2.py
+++ patrol_ros2.py
@@ -73,7 +73,7 @@ class SonarBot1(runner.HdxNode):
                 self.publisher.publish(self.forward)
         elif act_loc < 0.0:
             self.publisher.publish(self.turn_left)
-            if act_dir > 0.0:
+            if act_dir > -0.2:
                 self.publisher.publish(self.forward)
         else:
             if self.ir_sense:
