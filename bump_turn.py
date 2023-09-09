fbef9eefa20565d047c55941e21efc101b019613
KhounborineII <khounborineii@hendrix.edu>
2023-09-09 18:35:17 -0500

    testing patrol

--- bump_turn.py
+++ bump_turn.py
@@ -5,34 +5,36 @@ import rclpy
 from irobot_create_msgs.msg import HazardDetectionVector, WheelStatus, WheelTicks
 from rclpy.qos import qos_profile_sensor_data
 from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
 
 from action_demo import RotateActionClient
 
 
-class BumpTurnBot(runner.HdxNode):
+class PatrollerBot(runner.HdxNode):
     def __init__(self, namespace: str = ""):
-        super().__init__('bump_turn_bot')
+        super().__init__('patrol_bot')
         self.publisher = self.create_publisher(Twist, namespace + '/cmd_vel', 10)
         self.bumps = self.create_subscription(HazardDetectionVector, f"{namespace}/hazard_detection", self.bump_callback, qos_profile_sensor_data)
         self.wheel_status = self.create_subscription(WheelStatus, f'{namespace}/wheel_status', self.wheel_status_callback, qos_profile_sensor_data)
+        self.location = self.create_subscription(Odometry, namespace + '/odom', self.odom_callback, qos_profile_sensor_data)
         self.bump = None
         self.last_wheel_status = None
         self.rotator = RotateActionClient(self.turn_finished_callback, namespace)
+        self.true_loc = 0.0
 
     def wheels_stopped(self):
         return self.last_wheel_status is not None and self.last_wheel_status.current_ma_left == 0 and self.last_wheel_status.current_ma_right == 0
 
-    def bump_callback(self, msg):
-        self.record_first_callback()
-        if self.bump is None:
-            print("open")
-            self.bump = runner.find_bump_from(msg.detections)
-            if self.bump is None:
-                self.publisher.publish(runner.straight_twist(0.5))
+    def odom_callback(self, msg):
+        loc = msg.pose.pose.position.y
+        if self.true_loc == 0.0:
+            self.true_loc = loc
+        act_loc = loc - self.true_loc
+        print(f"position: {act_loc}")
+        if act_loc > 1.0 or act_loc < 0.0:
+            self.publisher.publish(runner.straight_twist(0.0))
         elif self.wheels_stopped():
-            goal = math.pi / 2
-            if 'left' in self.bump:
-                goal *= -1
+            goal = math.pi
             print("Starting turn", goal)
             self.rotator.send_goal(goal)
             rclpy.spin_once(self.rotator)
@@ -49,4 +51,4 @@ class BumpTurnBot(runner.HdxNode):
 
 
 if __name__ == '__main__':
-    runner.run_single_node(lambda: BumpTurnBot(f'/{sys.argv[1]}'))
+    runner.run_single_node(lambda: PatrollerBot(f'/{sys.argv[1]}'))
