b94f3610c2ec8e9b4a67b19a51a1c7c25a5d4f46
KhounborineII <khounborineii@hendrix.edu>
2023-09-09 19:12:17 -0500

    bump_turn added again

--- bump_turn.py
+++ bump_turn.py
@@ -2,64 +2,79 @@ import sys
 import math
 import runner
 import rclpy
-from irobot_create_msgs.msg import HazardDetectionVector, WheelStatus, WheelTicks
+from irobot_create_msgs.msg import HazardDetectionVector
 from rclpy.qos import qos_profile_sensor_data
 from geometry_msgs.msg import Twist
-from nav_msgs.msg import Odometry
 
 from action_demo import RotateActionClient
 
 
-class PatrollerBot(runner.HdxNode):
-    def __init__(self, namespace: str = ""):
-        super().__init__('patrol_bot')
-        self.publisher = self.create_publisher(Twist, namespace + '/cmd_vel', 10)
-        self.wheel_status = self.create_subscription(WheelStatus, f'{namespace}/wheel_status', self.wheel_status_callback, qos_profile_sensor_data)
-        self.location = self.create_subscription(Odometry, namespace + '/odom', self.odom_callback, qos_profile_sensor_data)
-        self.last_wheel_status = None
+class BumpTurnNode(runner.HdxNode):
+    def __init__(self, namespace: str = "", avoid_angle=math.pi / 2):
+        super().__init__('bump_turn_node')
+        self.bumps = self.create_subscription(HazardDetectionVector, f"{namespace}/hazard_detection", self.bump_callback, qos_profile_sensor_data)
+        self.avoid_angle = avoid_angle
         self.rotator = RotateActionClient(self.turn_finished_callback, namespace)
-        self.true_loc = 0.0
-        self.wheel_turn = False
-        self.spin_wheel = False
+        self.turning = False
+        self.bump = None
+        self.started = False
 
-    def wheels_stopped(self):
-        return self.last_wheel_status is not None and self.last_wheel_status.current_ma_left == 0 and self.last_wheel_status.current_ma_right == 0
+    def has_started(self):
+        return self.started
+        
+    def bump_clear(self):
+        return self.bump is None
 
-    def odom_callback(self, msg):
-        loc = msg.pose.pose.position.y
-        if self.true_loc == 0.0:
-            self.true_loc = loc
-        act_loc = loc - self.true_loc
-        print(f"position: {act_loc}")
-        if not self.wheel_turn:
-            if act_loc > 1.0 or act_loc < 0.0:
-                self.publisher.publish(runner.straight_twist(0.0))
-                self.wheel_turn = True
-            elif act_loc < 1.0 and act_loc > 0.0:
-                self.publisher.publish(runner.straight_twist(0.5))
-            else:
-                print("waiting on wheels")
-        else:
-            if not self.spin_wheel:
-                goal = math.pi
-                print("Starting turn", goal)
-                self.rotator.send_goal(goal)
-                rclpy.spin_once(self.rotator)
-            elif self.spin_wheel:
-                self.publisher.publish(runner.straight_twist(0.5))
-            if act_loc < 1.0 and act_loc > 0.0:
-                self.wheel_turn = False
-                self.spin_wheel = False
+    def is_turning(self):
+        return self.turning
+
+    def bump_callback(self, msg):
+        self.record_first_callback()
+        self.started = True
+        if self.bump is None and not self.turning:
+            self.bump = runner.find_bump_from(msg.detections)
+            if self.bump:
+                print(f"Detected {self.bump}")
             
+    def start_turn(self):
+        goal = self.avoid_angle
+        if 'left' in self.bump:
+            goal *= -1
+        self.rotator.send_goal(goal)
+        self.turning = True
+        print(f"Sending goal: {goal}")
 
     def turn_finished_callback(self, future):
-        self.spin_wheel = True
-        print("finished with turn")
+        print("BumpTurnNode: Turn finished")
+        self.bump = None
+        self.turning = False
 
-    def wheel_status_callback(self, msg):
+    def add_self_recursive(self, executor):
+        executor.add_node(self)
+        executor.add_node(self.rotator)
+
+
+class BumpTurnBot(runner.WheelMonitorNode):
+    def __init__(self, namespace: str = ""):
+        super().__init__('bump_turn_bot', namespace)
+        self.publisher = self.create_publisher(Twist, namespace + '/cmd_vel', 10)
+        self.bump_node = BumpTurnNode(namespace)
+        self.create_timer(0.10, self.timer_callback)
+
+    def timer_callback(self):
         self.record_first_callback()
-        self.last_wheel_status = msg
+        if self.bump_node.has_started() and not self.bump_node.is_turning():
+            if self.bump_node.bump_clear():
+                self.publisher.publish(runner.straight_twist(0.5))
+            elif self.wheels_stopped():
+                self.bump_node.start_turn()
+
+    def add_self_recursive(self, executor):
+        executor.add_node(self)
+        self.bump_node.add_self_recursive(executor)
 
 
 if __name__ == '__main__':
-    runner.run_single_node(lambda: PatrollerBot(f'/{sys.argv[1]}'))
+    rclpy.init()
+    bot = BumpTurnBot(f'/{sys.argv[1]}')
+    runner.run_recursive_node(bot)
