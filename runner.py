e086a8565e5cc117767bc5016bb503292ec767e2
KhounborineII <khounborineii@hendrix.edu>
2023-10-19 17:11:54 -0500

    Runner Update

--- runner.py
+++ runner.py
@@ -7,6 +7,11 @@ import cv2
 import math
 
 from geometry_msgs.msg import Twist
+from irobot_create_msgs.msg import WheelStatus
+from rclpy.qos import qos_profile_sensor_data
+
+from rclpy.action import ActionClient
+from irobot_create_msgs.action import RotateAngle, DriveDistance
 
 
 def straight_twist(vel):
@@ -58,7 +63,7 @@ class HdxNode(Node):
     def record_first_callback(self):
         if self.first_callback_time is None:
             self.first_callback_time = self.elapsed_time()
-            print(f"ROS2 active at {self.first_callback_time}")
+            print(f"First ROS2 callback for {self.get_name()} at {self.first_callback_time}")
 
     def ros_issuing_callbacks(self):
         return self.first_callback_time is not None
@@ -74,6 +79,20 @@ class HdxNode(Node):
 
     def quitting(self):
         return self.done
+
+
+class WheelMonitorNode(HdxNode):
+    def __init__(self, name, namespace):
+        super().__init__(name)
+        self.wheel_status = self.create_subscription(WheelStatus, f'{namespace}/wheel_status', self.wheel_status_callback, qos_profile_sensor_data)
+        self.last_wheel_status = None
+
+    def wheels_stopped(self):
+        return self.last_wheel_status is not None and self.last_wheel_status.current_ma_left == 0 and self.last_wheel_status.current_ma_right == 0
+
+    def wheel_status_callback(self, msg):
+        self.record_first_callback()
+        self.last_wheel_status = msg
 
 
 def run_single_node(node_maker):
@@ -150,6 +169,7 @@ def input_thread(finished, ros_ready):
     ros_ready.wait()
     user = input("Type anything to exit")
     finished.set()
+    rclpy.shutdown()
 
 
 def spin_thread(finished, ros_ready, node_maker):
@@ -168,3 +188,112 @@ def spin_thread(finished, ros_ready, node_maker):
     node.reset()
     rclpy.shutdown()
     print("ROS2 has shut down")
+
+
+# Inspired by: https://answers.ros.org/question/377848/spinning-multiple-nodes-across-multiple-threads/?answer=377861#post-id-377861
+# call rclpy.init() before invoking
+def run_multiple_nodes(*nodes):
+    executor = rclpy.executors.MultiThreadedExecutor()
+    for node in nodes:
+        executor.add_node(node)
+    executor_thread = threading.Thread(target=executor.spin, daemon=True)
+    executor_thread.start()
+    user = input("Type anything to exit")
+    rclpy.shutdown()
+    executor_thread.join()
+
+
+# call rclpy.init() before invoking
+def run_recursive_node(recursive_node):
+    executor = rclpy.executors.MultiThreadedExecutor()
+    recursive_node.add_self_recursive(executor)
+    executor_thread = threading.Thread(target=executor.spin, daemon=True)
+    executor_thread.start()
+    user = input("Type anything to exit")
+    rclpy.shutdown()
+    executor_thread.join()
+
+
+# call rclpy.init() before invoking
+def run_recursive_vision_node(cv_object, recursive_node):
+    finished = threading.Event()
+    vt = threading.Thread(target=lambda cv: cv.loop(finished), args=(cv_object,))
+    vt.start()
+    executor = rclpy.executors.MultiThreadedExecutor()
+    recursive_node.add_self_recursive(executor)
+    executor_thread = threading.Thread(target=executor.spin, daemon=True)
+    executor_thread.start()
+    vt.join()
+    rclpy.shutdown()
+    executor_thread.join()
+
+
+# call rclpy.init() before invoking
+def run_vision_multiple_nodes(cv_object, *nodes):
+    finished = threading.Event()
+    vt = threading.Thread(target=lambda cv: cv.loop(finished), args=(cv_object,))
+    vt.start()
+    executor = rclpy.executors.MultiThreadedExecutor()
+    for node in nodes:
+        executor.add_node(node)
+    executor_thread = threading.Thread(target=executor.spin, daemon=True)
+    executor_thread.start()
+    vt.join()
+    rclpy.shutdown()
+    executor_thread.join()
+
+
+class CustomActionClient(Node):
+    def __init__(self, name, py_action_type, ros2_action_type, callback, namespace):
+        super().__init__(name)
+        self._action_client = ActionClient(self, py_action_type, f'{namespace}/{ros2_action_type}')
+        self.callback = callback
+        self.goal_handle = None
+
+    def send_goal(self, goal_msg):
+        self._action_client.wait_for_server()
+        future = self._action_client.send_goal_async(goal_msg)
+        future.add_done_callback(self.goal_response_callback)
+
+    def goal_response_callback(self, future):
+        self.goal_handle = future.result()
+        if self.goal_handle.accepted:
+            print("Goal accepted.")
+            self._get_result_future = self.goal_handle.get_result_async()
+            self._get_result_future.add_done_callback(self.callback)
+        else:
+            print("Goal rejected...")
+
+    def cancel(self):
+        if self.goal_handle is not None:
+            future = self.goal_handle.cancel_goal()
+            if future:
+                print("Goal cancelled")
+            else:
+                print("Failed to cancel goal")
+
+    def spin_thread(self):
+        st = threading.Thread(target=lambda ac: rclpy.spin(ac), args=(self,))
+        st.start()
+
+
+class RotateActionClient(CustomActionClient):
+    def __init__(self, callback, namespace):
+        super().__init__("RotateActionClient", RotateAngle, 'rotate_angle', callback, namespace)
+        
+    def send_goal(self, goal_heading, radians_per_sec=1.0):
+        goal_msg = RotateAngle.Goal()
+        goal_msg.angle = goal_heading
+        goal_msg.max_rotation_speed = radians_per_sec
+        super().send_goal(goal_msg)
+
+
+class DriveDistanceClient(CustomActionClient):
+    def __init__(self, callback, namespace):
+        super().__init__("DriveDistanceClient", DriveDistance, 'drive_distance', callback, namespace)
+
+    def send_goal(self, goal_distance, meters_per_sec=0.3):
+        goal_msg = DriveDistance.Goal()
+        goal_msg.distance = goal_distance
+        goal_msg.max_translation_speed = meters_per_sec
+        super().send_goal(goal_msg)
